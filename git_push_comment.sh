256fa426c60a42c1243ce4363576170d1f45c630
Beach, Marcus Lee <marcus.beach@ge.com>
2024-04-29 20:51:53 -0400

    Update git_push_comment.sh:

--- git_push_comment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-changes=$(git diff --cached --name-status)
-commit_message=$(echo "$changes" | awk '{ if ($1 == "A") { print "Added: "$2 } else if ($1 == "M") { print "Modified: "$2 } else if ($1 == "D") { print "Deleted: "$2 } }' | awk 'BEGIN{ORS="\n"} {print "- " $0}')
-git commit -m "$commit_message"
-git push origin "$current_branch"
