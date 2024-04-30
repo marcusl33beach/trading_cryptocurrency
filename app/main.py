f4449a3a510c8b044efe648439e5cd3d76a3d958
Beach, Marcus Lee <marcus.beach@ge.com>
2024-04-30 07:24:21 -0400

    app/main.py:

--- app/main.py
+++ app/main.py
@@ -5,10 +5,13 @@ import pandas as pd
 
 
 def main():
+    # Get the cryptocurrency data
     cryptocurrency = get_coin_data(api_key(), 'BTC')
     # Read the data dict into a DataFrame
     cryptocurrencyd_dataframe = pd.DataFrame(cryptocurrency, index=[1])
+    # Print the DataFrame
     print(cryptocurrencyd_dataframe)
+    # Write the DataFrame to a CSV file
     write_to_file('/data/cryptocurrency.csv', cryptocurrencyd_dataframe)
 
 
